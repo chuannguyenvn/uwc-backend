378505801056531245998af84a1160b0c75907be
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-09 16:53:34 +0700

    feat: login role segregation

--- SharedLibrary/Communications/Authentication/Login.cs
+++ SharedLibrary/Communications/Authentication/Login.cs
@@ -4,6 +4,8 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public bool IsFromDesktop { get; set; }
     }
 
     public class LoginResponse
