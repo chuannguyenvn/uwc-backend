33544541e3969bc0ef836a9559521451921c6859
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-15 23:40:28 +0700

    feat: task controller

---
+++ SharedLibrary/Endpoints/Endpoints.TaskData.cs
@@ -0,0 +1,16 @@
+﻿namespace Requests
+{
+    public static partial class Endpoints
+    {
+        public static class TaskData
+        {
+            private const string TASK_DATA = "taskdata";
+            
+            public const string ADD_TASK = "add-task";
+            public const string COMPLETE_TASK = "complete-task";
+            
+            public static string AddTask => DOMAIN + "/" + TASK_DATA + "/" + ADD_TASK;
+            public static string CompleteTask => DOMAIN + "/" + TASK_DATA + "/" + COMPLETE_TASK;
+        }
+    }
+}
