e2264356902359700e44af2e91c99aa18420cde6
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-16 19:58:37 +0700

    feat: switching between properties and fields

--- SharedLibrary/Communications/Authentication/Login.cs
+++ SharedLibrary/Communications/Authentication/Login.cs
@@ -2,10 +2,21 @@
 {
     public class LoginRequest
     {
+#if NET7_0
         public string Username { get; set; }
         public string Password { get; set; }
 
         public bool IsFromDesktop { get; set; }
+#else
+        public string Username;
+        public string Password;
+
+        public bool IsFromDesktop;
+
+        public LoginRequest()
+        {
+        }
+#endif
     }
 
     public class LoginResponse
