e3ca35c55d9831d790c532c7c9e207cd06ebed47
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-16 19:57:46 +0700

    feat: allow null fields

--- Program.cs
+++ Program.cs
@@ -69,7 +69,7 @@ builder.Services.AddCors(options =>
 
 #endregion
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
