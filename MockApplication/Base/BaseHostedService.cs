c962c5175aaeb974b84031abfac97f762cbcd016
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-25 21:30:09 +0700

    feat: basic structure

---
+++ MockApplication/Base/BaseHostedService.cs
@@ -0,0 +1,16 @@
+﻿namespace MockApplication.Base;
+
+public abstract class BaseHostedService : IHostedService
+{
+    protected abstract Task Main();
+    
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Main();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ MockApplication/McpFilling/McpFillingMock.cs
@@ -0,0 +1,16 @@
+﻿using MockApplication.Base;
+
+namespace MockApplication.McpFilling;
+
+public class McpFillingMock : BaseHostedService
+{
+    protected override async Task Main()
+    {
+        while (true)
+        {
+            // Do something here...
+
+            await Task.Delay(1000);
+        }
+    }
+}
