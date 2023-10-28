97dc8f47bcf6ed6d0508952b9c365957b9e26adf
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-28 18:22:33 +0700

    feat: report controller

---
+++ Core/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Commons.Endpoints;
+using Microsoft.AspNetCore.Authorization;
+using Services.Reports;
+
+namespace Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class ReportController : Controller
+{
+    private readonly IReportService _reportService;
+
+    public ReportController(IReportService reportService)
+    {
+        _reportService = reportService;
+    }
+
+    [HttpGet(Endpoints.Report.GET)]
+    public IActionResult Get()
+    {
+        var result = _reportService.GetTodayDashboardReport();
+        return ProcessRequestResult(result);
+    }
+}
