8e6790ec5e56c76673e65d59cc9f4b3c074d69e0
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-12-06 02:18:05 +0700

    fix: assign worker to task

--- Core/Services/Tasks/TaskOptimizationServiceHelper.cs
+++ Core/Services/Tasks/TaskOptimizationServiceHelper.cs
@@ -133,7 +133,7 @@ public class TaskOptimizationServiceHelper
             };
 
             _unitOfWork.TaskDataDataRepository.Add(taskData);
-            
+
             if (isGrouped is false)
             {
                 CurrentTaskGroupId++;
@@ -194,7 +194,7 @@ public class TaskOptimizationServiceHelper
 
         _unitOfWork.Complete();
     }
-    
+
     public void UpdatePriority(int taskId, int priority)
     {
         if (!_unitOfWork.TaskDataDataRepository.DoesIdExist(taskId)) throw new Exception("Task not found");
@@ -212,8 +212,6 @@ public class TaskOptimizationServiceHelper
         {
             UpdatePriority(taskIds[i], i);
         }
-
-        _unitOfWork.Complete();
     }
 
     public void AssignWorkerToTask(int taskId, int workerId)
@@ -226,6 +224,8 @@ public class TaskOptimizationServiceHelper
         if (taskData.AssigneeId.HasValue) throw new Exception("Task already has a worker assigned");
 
         taskData.AssigneeId = workerId;
+        _unitOfWork.TaskDataDataRepository.Update(taskData);
+
         _unitOfWork.Complete();
 
         if (BaseHub.ConnectionIds.TryGetValue(workerId, out var connectionId))
@@ -249,7 +249,7 @@ public class TaskOptimizationServiceHelper
         {
             throw new ArgumentOutOfRangeException("Index 2 out of range");
         }
-        
+
         (list[index1], list[index2]) = (list[index2], list[index1]);
     }
 
@@ -303,7 +303,7 @@ public class TaskOptimizationServiceHelper
         {
             index2--;
         }
-        
+
         SwapLists(arr, index1 - 1, index2);
 
         index2 = arr.Count - 1;
